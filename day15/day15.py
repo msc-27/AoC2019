b2c8affd0fbaa53904f9bd70c10453113a6fa349
Mark <mark@msc27.me.uk>
2019-12-16 11:57:48 +0000

    Day 15

---
+++ day15/day15.py
@@ -0,0 +1,53 @@
+import intcode
+import astar
+with open('input') as f:
+    lines = [x.strip() for x in f]
+csv = [x.split(',') for x in lines]
+prog = [int(x) for x in csv[0]]
+m = intcode.machine(prog)
+maze = dict()
+
+maze[(0,0)] = m # Install 'droid at starting location
+target = None
+
+def explore(p): # Find exits from location p
+    global target # set this if the oxygen system is found
+    move = {1:(0,1),2:(0,-1),3:(1,0),4:(-1,0)}
+    rv = []
+    x,y = p
+    m = maze[(x,y)] # Get the 'droid installed at p
+    for direction,reverse in zip([1,2,3,4],[2,1,4,3]):
+        m.put(direction)
+        m.run()
+        r = m.get()
+        if r in (1,2): # 'droid moved: add new location
+            nx = x + move[direction][0]
+            ny = y + move[direction][1]
+            rv.append(((nx,ny),1))
+            maze[(nx,ny)] = m.copy() # Clone the 'droid and leave it behind
+            m.put(reverse) # Move the 'droid back whence it came
+            m.run()
+            _ = m.get()
+            if r == 2:
+                target = (nx,ny)
+    return rv
+
+import astar
+a = astar.astar((0,0),explore)
+states = a.run(None) # Explore fully and find target
+solution = a.run(target) # Find path to target
+print(solution[0])
+
+Map = [x[0] for x in states]
+def trans(p): # Simple transition function now we have the map
+    rv = []
+    x,y = p
+    if (x,y+1) in Map: rv.append(((x,y+1),1))
+    if (x,y-1) in Map: rv.append(((x,y-1),1))
+    if (x+1,y) in Map: rv.append(((x+1,y),1))
+    if (x-1,y) in Map: rv.append(((x-1,y),1))
+    return rv
+
+a = astar.astar(target,trans) # Part 2: target is now the initial state
+solution = a.run(None) # Explore fully and get costs for all locations
+print(max(x[1] for x in solution))
---
+++ day15/intcode.py
@@ -0,0 +1,106 @@
+from collections import defaultdict
+class machine:
+    _ops = dict()
+    def __init__(self, values):
+        self.mem = defaultdict(int)
+        for i in range(len(values)): self.mem[i] = values[i] 
+        self.ip = 0
+        self.rb = 0
+        self.halted = False
+        self.blocked = False
+        self._inputs = []
+        self._outputs = []
+        self.debug = False
+        self.pause_on_output = False
+    def copy(self):
+        m = machine(self.mem)
+        m.ip = self.ip
+        m.rb = self.rb
+        m.halted = self.halted
+        m.blocked = self.blocked
+        m._inputs = self._inputs.copy()
+        m._outputs = self._outputs.copy()
+        m.debug = self.debug
+        m.pause_on_output = self.pause_on_output
+        return m
+    def get(self):
+        return self._outputs.pop(0) if self._outputs else None
+    def put(self, x):
+        self._inputs.append(x)
+    def peek(self, addr):
+        return self.mem[addr]
+    def poke(self, addr, val):
+        self.mem[addr] = val
+    def step(self):
+        if self.debug: self._debug_print()
+        if not self.halted:
+            op = self.mem[self.ip]
+            self.ip += 1
+            self.modes = op // 100;
+            self._ops[op%100](self)
+    def run(self):
+        self.blocked = False
+        while not self.halted and \
+              not self.blocked and \
+              not (self.pause_on_output and self._outputs): self.step()
+    def _debug_print(self):
+        print('ip=',self.ip,' rb=',self.rb)
+        print(self.mem)
+
+    def _get_parms(self, n):
+        parms = []
+        for i in range(n):
+            p = self.mem[self.ip]
+            m = self.modes % 10
+            self.modes //= 10
+            if m == 1: p = self.ip
+            if m == 2: p += self.rb
+            parms.append(p)
+            self.ip += 1
+        return parms
+
+    def _add(self):
+        p1,p2,p3 = self._get_parms(3)
+        self.mem[p3] = self.mem[p1] + self.mem[p2]
+    def _mul(self):
+        p1,p2,p3 = self._get_parms(3)
+        self.mem[p3] = self.mem[p1] * self.mem[p2]
+    def _halt(self):
+        self.halted = True
+    def _cmpeq(self):
+        p1,p2,p3 = self._get_parms(3)
+        self.mem[p3] = 1 if self.mem[p1] == self.mem[p2] else 0
+    def _cmplt(self):
+        p1,p2,p3 = self._get_parms(3)
+        self.mem[p3] = 1 if self.mem[p1] < self.mem[p2] else 0
+    def _jc(self):
+        p1,p2 = self._get_parms(2)
+        if self.mem[p1]: self.ip = self.mem[p2]
+    def _jnc(self):
+        p1,p2 = self._get_parms(2)
+        if not self.mem[p1]: self.ip = self.mem[p2]
+    def _out(self):
+        p = self._get_parms(1)[0]
+        self._outputs.append(self.mem[p])
+    def _stor(self):
+        p = self._get_parms(1)[0]
+        if self._inputs:
+            self.mem[p] = self._inputs.pop(0)
+            self.blocked = False
+        else:
+            self.ip -= 2
+            self.blocked = True
+    def _setrb(self):
+        p = self._get_parms(1)[0]
+        self.rb += self.mem[p]
+
+    _ops[1] = _add
+    _ops[2] = _mul
+    _ops[3] = _stor
+    _ops[4] = _out
+    _ops[5] = _jc
+    _ops[6] = _jnc
+    _ops[7] = _cmplt
+    _ops[8] = _cmpeq
+    _ops[9] = _setrb
+    _ops[99] = _halt
